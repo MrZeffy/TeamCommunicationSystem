feb6f2f543ad55ef0655a52a5134389b348c5385
Daman Kumar <44134180+MrZeffy@users.noreply.github.com>
2021-02-03 20:50:52 +0530

    Added private keys for https

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const io = require('socket.io')(server);
 const clientIo = require('socket.io-client'); 
 const {v4: uuidV4} = require('uuid');
 const path = require('path');
+
+var privateKey  = fs.readFileSync('sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('sslcert/server.crt', 'utf8');
 var credentials = {key: privateKey, cert: certificate};
 const httpsServer = https.createServer(credentials, app);
 //const mySQLConnector = require('mysql');
